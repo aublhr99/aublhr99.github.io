f18c940979d2c8ee7fd72488d1678c4cda3fc5b0
Aubin C Lohier <aublhr99@gmail.com>
2024-12-16 13:00:30 -0800

    Added Todos and got rid of card background

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,35 +1,50 @@
 import "./App.css";
 import ParticlesBackground from "./ParticlesBackground";
-import { Card } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import Resume from "./assets/Aubin_Lohier_Resume.pdf";
+import ProfilePic from "./assets/Aubin_Lohier_profile_pic.jpeg";
 
 function App() {
   return (
     <div className="App">
       <ParticlesBackground />
       <div className="main-body">
-        <Card>
-          <h1 className="name">Aubin Lohier</h1>
-          <p id="tagline">"I'm interested in making connections."</p>
-          <p>Ex Meta/Facebook</p>
-          <p>Computer Scientist, Software Engineer, and Web Developer</p>
-          <div className="inline-btns">
-            <span className="btn">
-              <a href="https://www.github.com/aublhr99/">GitHub</a>
-            </span>
-            <span className="btn">
-              <a href="https://www.linkedin.com/in/aubin-lohier/">LinkedIn</a>
-            </span>
-            <span className="btn">
-              <a href="mailto:aublhr99@gmail.com">Email</a>
-            </span>
-            <span className="btn">
-              <a href={Resume} target="_blank" rel="noreferrer">
-                Resume
-              </a>
-            </span>
-          </div>
-        </Card>
+        <div className="card">
+          <Grid2 container spacing={6}>
+            <Grid2 item justifyContent="center" alignContent="center" size={4}>
+              <img src={ProfilePic} alt="Me" id="profile-pic" />
+            </Grid2>
+            <Grid2 item justifyItems="start" size={8}>
+              <h1 className="name">Aubin Lohier</h1>
+              <p id="tagline">
+                "I like building beautiful products that users love."
+              </p>
+              <p>Ex Meta/Facebook</p>
+              <p>
+                Builder, Computer Scientist, Software Engineer, and Web
+                Developer
+              </p>
+              <div className="inline-btns">
+                <span className="btn">
+                  <a href="https://www.github.com/aublhr99/">GitHub</a>
+                </span>
+                <span className="btn">
+                  <a href="https://www.linkedin.com/in/aubin-lohier/">
+                    LinkedIn
+                  </a>
+                </span>
+                <span className="btn">
+                  <a href="mailto:aublhr99@gmail.com">Email</a>
+                </span>
+                <span className="btn">
+                  <a href={Resume} target="_blank" rel="noreferrer">
+                    Resume
+                  </a>
+                </span>
+              </div>
+            </Grid2>
+          </Grid2>
+        </div>
       </div>
     </div>
   );
